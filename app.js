5897ba132665f8a1ba43c7572e51067512a9d546
usmanrafi110 <usmanrafi110@gmail.com>
2025-07-30 12:58:08 +0500

    Add cronjob and few changes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import express from "express";
 import { resolve } from "path";
 import { corsOptions } from "./src/constants/options.js";
 import errorMiddleware from "./src/middlewares/errorMiddleware.js";
+import cron from "node-cron";
+import fetch from "node-fetch";
 
 //* Routes:
 import { authRouter } from "./src/routes/auth.routes.js";
@@ -19,6 +21,20 @@ const rootDir = resolve(__dirname, "..");
 //   api_key: process.env.CLOUDINARY_API_KEY,
 //   api_secret: process.env.CLOUDINARY_API_SECRET,
 // });
+
+// Replace with your deployed app's public URL
+const APP_URL = "https://e-sign-backend.onrender.com/health";
+
+cron.schedule("*/10 * * * *", async () => {
+  try {
+    const res = await fetch(APP_URL);
+    console.log(
+      `Pinged self at ${new Date().toISOString()} - Status: ${res.status}`
+    );
+  } catch (err) {
+    console.error("Error pinging self:", err);
+  }
+});
 
 app.use(cors(corsOptions));
 app.use(compression());
--- server.js
+++ server.js
@@ -4,13 +4,10 @@ import { connectToDB } from "./src/config/dbConnection.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 // Add a health check endpoint
 app.get("/health", (req, res) => {
   res.json({ status: "OK", message: "Server is running" });
+  console.log("Health check endpoint hit");
 });
 
 // Server Listener
@@ -18,13 +15,10 @@ app.listen(PORT, async () => {
   try {
     await connectToDB();
     console.log(
-      `Server is running at port *${PORT} in *${process.env.NODE_ENV}Mode`
+      `Server is running at port *${PORT} in *${process.env.NODE_ENV} Mode`
     );
   } catch (error) {
     console.error("Failed to connect to database:", error);
-    console.log(
-      `Server is running at port *${PORT} in *${process.env.NODE_ENV}Mode (without database)`
-    );
   }
 });
 
