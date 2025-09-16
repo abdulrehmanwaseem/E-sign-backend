1ba327ecc481cf7d682be5afb103bb6f07402d65
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-09-16 15:45:11 +0500

    Enchance corsOptions & remove extra files

--- src/constants/options.js
+++ src/constants/options.js
@@ -8,13 +8,7 @@ const cookieOptions = {
 };
 
 const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
-  ],
+  origin: process.env.CLIENT_URL,
   credentials: true,
 };
 
